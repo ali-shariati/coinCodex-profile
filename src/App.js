dc4bef4ad1fa1e7507bed53d085dfacc8aac2918
Ali Shariati <seyed.ali.shariati@gmail.com>
2023-10-27 21:38:25 +0330

    full demo without api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+// import {Provider} from "react-redux";
+// import {BrowserRouter as Router} from "react-router-dom";
+// import {store} from './store'
+// import {AppRoute} from './routes'
+import {Home} from "./pages";
+import {Navbar} from "./components/header/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        // <Provider store={store}>
+        // <Router>
+        //     <AppRoute/>
+        // </Router>
+        // </Provider>
+        <>
+            <Navbar />
+            <Home  />
+        </>
+
+
+    );
 }
 
 export default App;
